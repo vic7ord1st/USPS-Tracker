834994c84b5e00d453be08e3f8b52642cfc9ea51
Vic7ord1st <okoroafor.victor@gmail.com>
2019-07-20 19:40:27 +0100

    Synced with the USPS API which returns XML results

--- server.js
+++ server.js
@@ -2,18 +2,29 @@ const express = require('express');
 
 const request = require('request');
 
+const xmltojson = require('xml-to-json-stream');
+const xmlParser = xmltojson({attributeMode:false});
+
 const app = express();
 
 app.use(express.static('public'));
 
 app.get("/tracking", (req, res) => {
-    let trackingId = req;
+    let trackingId = req.query.tracking;
 
-    let trackingUrl =  "http://production.shippingapis.com/ShippingAPI.dll?API=TrackV2&XML= <TrackRequest USERID="+process.env.USERID+"> <TrackID ID="+trackingID+"></TrackID> </TrackRequest>"
+     let trackingUrl =  "http://production.shippingapis.com/ShippingAPI.dll?API=TrackV2&XML= <TrackRequest USERID="+process.env.USERID+"> <TrackID ID="+trackingId+"></TrackID> </TrackRequest>"
     
-    request(trackingUrl, (err, res, body) => {
-        res.send(body);
-        console.log(err);
+    let result = request(trackingUrl, (err, res) => {
+        return res;
+    });
+
+    let jsonOutput = xmlParser.xmltojson(result, (err, json) => {
+        if(err){
+            console.log("it's an error");
+        }
+        else{
+            res.send(json);
+        }
     })
 
 })
