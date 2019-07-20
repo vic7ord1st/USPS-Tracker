0f7632f9a294400a6b7aa5a0f4ae076065f09a69
Vic7ord1st <okoroafor.victor@gmail.com>
2019-07-20 19:06:49 +0100

    first commit

--- server.js
+++ server.js
@@ -4,8 +4,18 @@ const request = require('request');
 
 const app = express();
 
+app.use(express.static('public'));
+
 app.get("/tracking", (req, res) => {
+    let trackingId = req;
+
+    let trackingUrl =  "http://production.shippingapis.com/ShippingAPI.dll?API=TrackV2&XML= <TrackRequest USERID="+process.env.USERID+"> <TrackID ID="+trackingID+"></TrackID> </TrackRequest>"
     
+    request(trackingUrl, (err, res, body) => {
+        res.send(body);
+        console.log(err);
+    })
+
 })
 app.listen(process.env.PORT, () => {
     console.log(`server is running`);
